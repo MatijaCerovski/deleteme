fbddf9a9aae350c0b036d485bd03e1aeb266226f
mcerovski <mcerovski@croz.net>
2019-04-26 13:46:01 +0200

    email validation regex

---
+++ src/ReadDateFromString.java
@@ -0,0 +1,22 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class ReadDateFromString {
+
+
+    public static void main(String[] args) throws ParseException {
+
+        String dateString = "2019-04-26T10:06:18.786+02:00";
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        Date date = dateFormat.parse(dateString);
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+
+        System.out.println(dateFormat.format(date));
+
+    }
+}
