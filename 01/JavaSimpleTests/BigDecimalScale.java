8b57a3b1574c2d17b685f1f2b6fcd246e932685a
mcerovski <mcerovski@croz.net>
2019-05-02 09:12:59 +0200

    nesto big decimal

---
+++ 01/JavaSimpleTests/BigDecimalScale.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigDecimalScale {
+
+    public static void main(String[] args) {
+        BigDecimal number1 = new BigDecimal("200.555555").setScale(3, RoundingMode.HALF_UP);
+        BigDecimal number2 = new BigDecimal(20).setScale(2, RoundingMode.HALF_UP);
+
+
+        System.out.println(number1.toString());
+        System.out.println(number2.toString());
+    }
+}
