137f269e376355786967aba396df14ea93f55bf8
mcerovski <mcerovski@croz.net>
2019-05-10 10:22:53 +0200

    csv commons init

--- 01/JavaSimpleTests/BigDecimalScale.java
+++ 01/JavaSimpleTests/BigDecimalScale.java
@@ -7,8 +7,12 @@ public class BigDecimalScale {
         BigDecimal number1 = new BigDecimal("200.555555").setScale(3, RoundingMode.HALF_UP);
         BigDecimal number2 = new BigDecimal(20).setScale(2, RoundingMode.HALF_UP);
 
+        BigDecimal number3 = new BigDecimal(20.55);
+
+
 
         System.out.println(number1.toString());
         System.out.println(number2.toString());
+        System.out.println(number3.toString());
     }
 }
